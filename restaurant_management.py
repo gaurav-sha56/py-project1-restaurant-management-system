63451226e5bd00624984f235685dc63a6913c9c1
Gaurav <sharmagaurav1956@gmail.com>
2024-12-07 23:44:04 +0530

    First python project

---
+++ restaurant_management.py
@@ -0,0 +1,31 @@
+# this code is for restaurant management sysytem and this is my first project
+# learning new things
+
+
+
+
+print("This is our menu: ")
+print("Pizza -> 500")
+print("Burger -> 250")
+print("noodles -> 120")
+print("pasta -> 200")
+print("sandwich -> 70")
+print("colddrink -> 50")
+print("coffee -> 60")
+print("tea -> 50")
+
+
+bill=0
+rate = {"pizza":500, "burger":250, "noodles":120,"pasta":200,"sandwich":70,"colddrink":50,"coffee":60,"tea":50}
+customer = True
+while customer:
+    item = input("Your order: ")
+    order = item.split(" ")
+    for food in order:
+        bill+=rate[food.lower()]
+    choice=input("you want to order more?")
+    if choice.lower()=="no":
+        customer=False
+
+
+print(f"Your bill is {bill}\nThank you for visiting")
